1e166738b1ea32fe2797657379d2d55576dde50e
wtorz <1500804449@qq.com>
2017-11-16 21:27:57 +0800

    wt

---
+++ src/main/java/com/xawl/travel/controller/BusinessImgController.java
@@ -0,0 +1,13 @@
+package com.xawl.travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/11/16.
+ */
+
+@Controller
+@RequestMapping("/")
+public class BusinessImgController {
+}
