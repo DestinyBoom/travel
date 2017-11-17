8b0eaf20aba0bfc985568760d089619ab685f544
wtorz <1500804449@qq.com>
2017-11-17 14:32:38 +0800

    wt

--- src/main/java/com/xawl/travel/controller/BusinessImgController.java
+++ src/main/java/com/xawl/travel/controller/BusinessImgController.java
@@ -1,13 +1,31 @@
 package com.xawl.travel.controller;
 
+import com.xawl.travel.service.BusinessImgService;
+import com.xawl.travel.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Administrator on 2017/11/16.
  */
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/businessImg")
 public class BusinessImgController {
+
+    @Autowired
+    private BusinessImgService businessImgService;
+
+
+    @ResponseBody
+    @RequestMapping("/selectImgById.action")
+    public Result selectImgById(HttpServletRequest request, HttpServletResponse response){
+
+        return null;
+    }
 }
---
+++ src/main/java/com/xawl/travel/service/BusinessImgService.java
@@ -0,0 +1,7 @@
+package com.xawl.travel.service;
+
+/**
+ * Created by Administrator on 2017/11/17.
+ */
+public class BusinessImgService {
+}
