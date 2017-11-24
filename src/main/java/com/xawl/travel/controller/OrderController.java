af85163c572b7e1c571cbe5e8834fc3adfa429da
wtorz <1500804449@qq.com>
2017-11-24 18:58:19 +0800

    商家订单多种查询

--- src/main/java/com/xawl/travel/controller/OrderController.java
+++ src/main/java/com/xawl/travel/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.xawl.travel.controller;
 
+import com.xawl.travel.pojo.Order;
 import com.xawl.travel.service.OrderService;
 import com.xawl.travel.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Created by Administrator on 2017/11/22.
@@ -19,11 +21,54 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+
+    /**
+     * 根据商家id查询订单
+     * @param bid
+     * @param pn
+     * @return
+     * @throws Exception
+     */
     @ResponseBody
     @RequestMapping("/selectOrderByBid.action")
     public Result selectOrderByBid(@RequestParam("bid") String bid, @RequestParam(value = "pn", defaultValue = "1") Integer pn) throws Exception {
 
         Result result = orderService.selectOrderByBid(bid, pn);
+        return result;
+    }
+
+
+    /**
+     * 根据订单号查询
+     * @param oid
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequestMapping("/selectOrderByOid.action")
+    public Result selectOrderByOid(@RequestParam("oid") String oid) throws Exception {
+
+        Result result = orderService.selectOrderByOid(oid);
+        return result;
+    }
+
+
+    /**
+     * 通过订单状态查询订单
+     * @param order
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequestMapping("/selectOrderByStatus.action")
+    public Result selectOrderByStatus(Order order) throws Exception {
+
+        /*HashMap<String,String> maps = new HashMap<String, String>();
+        maps.put("status",status);
+        maps.put("bid",bid);*/
+        order.setBid(order.getBid());
+        order.setStatus(order.getStatus());
+        Result result = orderService.selectOrderByStatus(order);
         return result;
     }
 
--- src/main/java/com/xawl/travel/service/OrderService.java
+++ src/main/java/com/xawl/travel/service/OrderService.java
@@ -9,6 +9,7 @@ import com.xawl.travel.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -30,6 +31,33 @@ public class OrderService {
             List<Order> businessOrderList = orderMapper.selectOrderByBid(bid);
             PageInfo<Order> pageInfo = new PageInfo<Order>(businessOrderList);
             return Result.success(pageInfo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("查询失败");
+        }
+    }
+
+    public Result selectOrderByOid(String oid) {
+        if (oid == null || oid == "") {
+            return Result.fail(300, "请给出对应订单号");
+        }
+        try {
+            Order order = orderMapper.selectByPrimaryKey(oid);
+            return Result.success(order);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("查询失败");
+        }
+    }
+
+    public Result selectOrderByStatus(Order order) {
+        
+        if (order.getBid() == null||order.getBid()==""){
+            return Result.fail(300, "查询对象不存在");
+        }
+        try {
+            List<Order> orderList = orderMapper.selectOrderByStatus(order);
+            return Result.success(orderList);
         } catch (Exception e) {
             e.printStackTrace();
             return Result.fail("查询失败");
