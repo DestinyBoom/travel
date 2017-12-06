2af1d8be3dd553905b4b1b7f01c8e80adf3b2342
DT0814 <729742011@qq.com>
2017-12-06 10:59:24 +0800

    DT

--- src/main/java/com/xawl/travel/controller/BusinessController.java
+++ src/main/java/com/xawl/travel/controller/BusinessController.java
@@ -2,13 +2,13 @@ package com.xawl.travel.controller;
 
 import com.xawl.travel.pojo.Business;
 import com.xawl.travel.service.BusinessService;
-import com.xawl.travel.utils.CreateId;
 import com.xawl.travel.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
